1d90b98618f33daf82ada4608879bc6824e068b0
ekodeko <ed.arm7@gmail.com>
2025-08-05 14:52:29 +0700

    ref: route: penjelasan soal no 3.

--- routes/api.php
+++ routes/api.php
@@ -18,6 +18,20 @@ Route::prefix('products')->group(function () {
 });
 
 Route::get('soal_3', [ProductController::class, 'soal_3']);
+// refactor function 
+// function d(p, t) {
+    // if (t === 'GOLD') {
+    // return p * 0.8;
+    // } else if (t === 'SILVER') {
+    // return p * 0.9;
+    // } else {
+    // return p;
+// }
+// }
+// nama fungsi menjadi discount agar lebih readabale
+// parameter fungsi menjadi price, tier agar lebih readabale
+// untuk value statis dan fix seperti GOLD dan SILVER
+// lebih optimal menggunakan switch case statement di banding if statement agar lebih readable
 Route::get('soal_4', function (){
     $input = [1,2,3,4,5];
     $target = 6;
